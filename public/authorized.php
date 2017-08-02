019c7fb6ad7c9086c8e699c54f2250ca8a1a9193
Eric Pharr <ericjpharr@gmail.com>
2017-08-02 11:20:03 -0500

    Added/completed php POST exercise.

---
+++ public/authorized.php
@@ -0,0 +1,14 @@
+<?php
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Authorized</title>
+	</head>
+	<body>
+		<h1>Authorized</h1>
+	</body>
+</html>
---
+++ public/login.php
@@ -0,0 +1,45 @@
+<?php
+function pageController(){
+
+	$login = "";
+
+	$username = isset($_POST['username']) ? $_POST['username'] : "";
+
+	$password = isset($_POST['password']) ? $_POST['password'] : "";
+
+	if (!empty($username) && !empty($password)){
+		if ($username == 'guest' && $password == 'password'){
+			header("Location: http://codeup.dev/authorized.php");
+			die();
+		} else {
+			$login .= "Sorry, wrong username or password :(";
+		}
+	}
+
+	return[
+		'login' => $login
+	];
+}
+
+extract(pageController());
+
+ ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login Page</title>
+	</head>
+	<body>
+
+		<h1>Login</h1>
+		<form method="post">
+			<input type="text" name="username" placeholder="Enter username" value="">
+			<input type="password" name="password" placeholder="" value="">
+			<button type="submit">Submit</button>
+		</form>
+
+		<h4><?= $login ?></h4>
+
+	</body>
+</html>
